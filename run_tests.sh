decd6bee8b79c73f78ab36f2df9a6ceaa6c60b2d
Tianyi Li <tianyikillua@gmail.com>
2018-05-30 19:17:32 +0200

    working on a parallel version

--- run_tests.sh
+++ run_tests.sh
@@ -3,11 +3,12 @@ TEST_DIR=/home/aster/test
 
 mkdir $TEST_DIR
 cd $TEST_DIR
-$ASRUN --stdout=out --stderr=err --list --all --output=testcases >> screen
+$ASRUN --stdout=out --stderr=err --list --all --output=testcases > screen
 
 for testcase in `cat testcases`
 do
-    $ASRUN --stdout=out --stderr=err --test $testcase $TEST_DIR >> screen
+    echo "Working on $testcase..."
+    $ASRUN --stdout=out --stderr=err --test $testcase $TEST_DIR > screen
 done
 
 $ASRUN --stderr=err --diag --only_nook --astest_dir=$TEST_DIR
