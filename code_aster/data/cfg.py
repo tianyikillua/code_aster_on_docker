df8c962c3e1de38ce1b2d3747ba9a23f1e8b6041
Tianyi Li <tianyikillua@gmail.com>
2018-06-06 16:33:12 +0200

    cosmetics

--- code_aster/data/cfg.py
+++ code_aster/data/cfg.py
@@ -1,3 +1,3 @@
 def configure(self):
     self.env.append_value('LIB_METIS', ('parmetis'))
-    self.env.append_value('LIB_SCOTCH', ('ptscotch','ptscotcherr','ptscotcherrexit'))
+    self.env.append_value('LIB_SCOTCH', ('ptscotch', 'ptscotcherr', 'ptscotcherrexit'))
